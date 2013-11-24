37e680227bc495f84402bc61f9366c380c02bc68
andikacsui <andika.csui@gmail.com>
2013-11-24 14:05:26 +0700

    listview listener

--- src/project/TheExplorer/View/CustomizedDaftarMisi.java
+++ src/project/TheExplorer/View/CustomizedDaftarMisi.java
@@ -10,12 +10,15 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import project.TheExplorer.Controller.MisiHelper;
@@ -28,22 +31,28 @@ public class CustomizedDaftarMisi extends Activity {
 	// XML node keys
 	static final String KEY_SONG = "song"; // parent node
 	static final String KEY_ID = "id";
+	static final String KEY_ID_MISI = "idmisi";
 	static final String KEY_NAMA = "nama";
 	static final String KEY_LOKASI = "lokasi";
 	static final String KEY_DESKRIPSI = "deskripsi";
 	static final String KEY_FOTO = "foto";
 	ArrayList<Misi> daftarMisi;
-
+	static int id_misi;
 	ListView list;
 	ListMisiAdapter adapter;
 	Context context;
+	EditText IDMisi;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_daftar_misi);
 		context = this;
-
+		
+		if(daftarMisi != null){
+			daftarMisi.clear();
+		}
+		
 		ArrayList<HashMap<String, String>> missionList = new ArrayList<HashMap<String, String>>();
 		try {
 
@@ -60,7 +69,7 @@ public class CustomizedDaftarMisi extends Activity {
 				map.put(KEY_LOKASI, daftarMisi.get(i).getLokasi());
 				map.put(KEY_DESKRIPSI, daftarMisi.get(i).getDeskripsi());
 				map.put(KEY_FOTO, daftarMisi.get(i).getFoto());
-
+				map.put(KEY_ID_MISI, String.valueOf(daftarMisi.get(i).getID()));
 				// adding HashList to ArrayList
 				missionList.add(map);
 			}
@@ -70,17 +79,27 @@ public class CustomizedDaftarMisi extends Activity {
 			// Getting adapter by passing xml data ArrayList
 			adapter = new ListMisiAdapter(this, missionList);
 			list.setAdapter(adapter);
-
+			
 			// Click event for single list row
 			list.setOnItemClickListener(new OnItemClickListener() {
 
 				@Override
 				public void onItemClick(AdapterView<?> parent, View view,
 						int position, long id) {
-
+					AlertDialog.Builder dialog = new AlertDialog.Builder(CustomizedDaftarMisi.this);
+					dialog
+					.setTitle("Pilihan menu");
+					
 				}
 			});
 		} catch (Exception e) {
 		}
+	}
+	
+	public void TextViewDetailMisi_OnClick(View view) {
+		Intent nextScreen = new Intent(getApplicationContext(), CustomizedDaftarTempat.class);
+		id_misi = 5;
+		nextScreen.putExtra("IDMisi", 1);
+		startActivity(nextScreen); 
 	}
 }
