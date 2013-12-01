4bc89320d2d549bf9aa6359fff5e37a23512ba00
andikacsui <andika.csui@gmail.com>
2013-12-01 23:03:08 +0700

    benerin list tempast

--- src/project/TheExplorer/View/CustomizedDaftarTempat.java
+++ src/project/TheExplorer/View/CustomizedDaftarTempat.java
@@ -36,8 +36,7 @@ import project.TheExplorer.Controller.TempatHelper;
 import project.TheExplorer.Model.Misi;
 import project.TheExplorer.Model.Tempat;
 
-public class CustomizedDaftarTempat extends Activity implements
-		SensorEventListener {
+public class CustomizedDaftarTempat extends Activity {
 	// All static variables
 	static final String URL = "http://api.androidhive.info/music/music.xml";
 	// XML node keys
@@ -93,7 +92,7 @@ public class CustomizedDaftarTempat extends Activity implements
 
 			// log.d("hmmm", "haaah lala" + misiid);
 			daftarTempat = TempatHelper.GetListTempatByMisi(context,
-					ListViewSavedMission.idMisi);
+					ListViewActivity.idMisi);
 
 			// looping through all song nodes &lt;song&gt;
 			for (int i = 0; i < daftarTempat.size(); i++) {
@@ -120,20 +119,12 @@ public class CustomizedDaftarTempat extends Activity implements
 				// adding HashList to ArrayList
 				tempatList.add(map);
 			}
-<<<<<<< HEAD
-=======
 			daftarTempat.clear();
-<<<<<<< HEAD
->>>>>>> b249cc5bf9b9efd0b9381ba3bf38ca8c215f47f8
-=======
->>>>>>> b249cc5bf9b9efd0b9381ba3bf38ca8c215f47f8
 			list = (ListView) findViewById(R.id.list_tempat_layout);
 
 			// Getting adapter by passing xml data ArrayList
 			adapter = new ListTempatAdapter(this, tempatList);
 			list.setAdapter(adapter);
-
-
 
 			// Click event for single list row
 			list.setOnItemClickListener(new OnItemClickListener() {
@@ -147,154 +138,6 @@ public class CustomizedDaftarTempat extends Activity implements
 		} catch (Exception e) {
 		}
 
-		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		lastUpdate = System.currentTimeMillis();
 	}
 
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-			getAccelerometer(event);
-		}
-
-	}
-
-	private void getAccelerometer(SensorEvent event) {
-		float[] values = event.values;
-		// Movement
-		float x = values[0];
-		float y = values[1];
-		float z = values[2];
-
-		float accelationSquareRoot = (x * x + y * y + z * z)
-				/ (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
-		long actualTime = System.currentTimeMillis();
-		if (accelationSquareRoot >= 2) //
-		{
-			if (actualTime - lastUpdate < 200) {
-				return;
-			}
-			lastUpdate = actualTime;
-			/*
-			 * Toast.makeText(this, "Device was shuffed", Toast.LENGTH_SHORT)
-			 * .show(); if (color) { view.setBackgroundColor(Color.GREEN);
-			 * 
-			 * } else { view.setBackgroundColor(Color.RED); } color = !color;
-			 */
-
-			// create class object
-			gps = new GPSTracker(context);
-
-			// check if GPS enabled
-			if (gps.canGetLocation()) {
-
-				// \n is for new line
-				// Toast.makeText(getApplicationContext(),
-				// "" + locationMatching(), Toast.LENGTH_LONG).show();
-
-				locationMatching();
-
-			} else {
-				// can't get location
-				// GPS or Network is not enabled
-				// Ask user to enable GPS/network in settings
-				gps.showSettingsAlert();
-			}
-		}
-	}
-
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-		// register this class as a listener for the orientation and
-		// accelerometer sensors
-		sensorManager.registerListener(this,
-				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
-				SensorManager.SENSOR_DELAY_NORMAL);
-	}
-
-	@Override
-	protected void onPause() {
-		// unregister listener
-		super.onPause();
-		sensorManager.unregisterListener(this);
-	}
-
-	private void locationMatching() {
-		float distance = -1;
-		boolean match = false;
-		int idTempat = 0;
-
-		locDb = new Location("");
-		locNow = new Location("");
-		locNow.setLatitude(gps.getLatitude());
-		locNow.setLongitude(gps.getLongitude());
-
-		for (int i = 0; i < daftarTempat.size(); i++) {
-			locDb.setLatitude(daftarTempat.get(i).getLatitude());
-			locDb.setLongitude(daftarTempat.get(i).getLongitude());
-			distance = locNow.distanceTo(locDb);
-			if (distance <= 1000f) {
-				match = true;
-				namaTempat = daftarTempat.get(i).getNama();
-				idTempat = daftarTempat.get(i).getID();
-				break;
-			}
-		}
-
-		if (match == true) {
-			
-			TempatHelper.CheckInTempat(context, idTempat);
-			//Toast.makeText(getApplicationContext(),
-			//		"You have arrived in " + namaTempat, Toast.LENGTH_LONG)
-			//		.show();
-			
-			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-					context);
-
-			// set title
-			alertDialogBuilder.setTitle("Join Mission");
-
-			// set dialog message
-			alertDialogBuilder
-					.setMessage("You have arrived in " + namaTempat + "\nDo you want to" +
-							" share it in twitter?")
-					.setCancelable(false)
-					.setPositiveButton("Yes",
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int id) {
-									Intent nextScreen = new Intent(
-											getApplicationContext(),
-											ShareTwitter.class);
-									startActivity(nextScreen);
-									
-								}
-							})
-					.setNegativeButton("No",
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int id) {
-									// if this button is clicked, just close
-									// the dialog box and do nothing
-									dialog.cancel();
-								}
-							});
-
-			// create alert dialog
-			AlertDialog alertDialog = alertDialogBuilder.create();
-
-			// show it
-			alertDialog.show();
-
-		} else {
-			Toast.makeText(getApplicationContext(), "No match location",
-					Toast.LENGTH_LONG).show();
-		}
-	}
 }
