50fe0ec1a3161f02d4cedba92fcf2301138fe8f6
andikacsui <andika.csui@gmail.com>
2013-12-13 14:53:35 +0700

    submit bismillah

--- src/project/TheExplorer/View/BantuanSlider.java
+++ src/project/TheExplorer/View/BantuanSlider.java
@@ -7,6 +7,7 @@ import project.TheExplorer.Controller.R;
 import project.TheExplorer.Model.Misi;
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.app.AlertDialog;
 import android.os.Bundle;
@@ -104,6 +105,81 @@ public class BantuanSlider extends Activity implements ViewFactory {
 	    ImageView i = new ImageView(this);  
 	    i.setScaleType(ImageView.ScaleType.FIT_CENTER);
 	    return i;
+	}
+	
+	public static boolean notEmpty(ArrayList<Misi> s) {
+		return (s != null && s.size() > 0);
+	}
+	
+	public void showAlertbox(String erroMessage) {
+		AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+
+		// Setting Dialog Title
+		alertDialog.setTitle("Error Message");
+
+		// Setting Dialog Message
+		alertDialog.setMessage(erroMessage);
+
+		// Setting OK Button
+		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				// Write your code here to execute after dialog closed
+
+			}
+		});
+
+		// Showing Alert Message
+		alertDialog.show();
+
+	}
+	
+	/**
+	 * Event Handling for Individual menu item selected Identify single menu
+	 * item by it's id
+	 * */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case R.id.new_mission:
+			// Single menu item is selected do something
+			// Ex: launching new activity/screen or show alert message
+			ArrayList<Misi> ListMisi = MisiHelper.GetNotSavedMission(context);
+			if (notEmpty(ListMisi)) {
+				Intent nextScreen = new Intent(getApplicationContext(),
+						ListViewActivity.class);
+				startActivity(nextScreen);
+			} else {
+				showAlertbox("All missions have been saved");
+			}
+			return true;
+
+		case R.id.saved_mission:
+			ArrayList<Misi> ListMisi2 = MisiHelper.GetSavedMission(context);
+			if (notEmpty(ListMisi2)) {
+				Intent nextScreen = new Intent(getApplicationContext(),
+						ListViewSavedMission.class);
+				startActivity(nextScreen);
+			} else {
+				showAlertbox("You don't have saved mission");
+			}
+			return true;
+
+		case R.id.achievement:
+			Intent nextScreen = new Intent(getApplicationContext(),
+					TabProfile.class);
+			startActivity(nextScreen);
+			return true;
+			
+		case R.id.help:
+			Intent nextScreen2 = new Intent(getApplicationContext(),
+					BantuanSlider.class);
+			startActivity(nextScreen2);
+			return true;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 	
 }
